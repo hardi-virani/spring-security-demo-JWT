6d72f165554ed27e976d7dedf0d3f20a3bde99c4
Hardi <hardivirani2003@gmail.com>
2026-07-04 21:34:30 -0400

    vid : 172 - Token Generated

--- src/main/java/com/star/spring_security_demo/service/JwtService.java
+++ src/main/java/com/star/spring_security_demo/service/JwtService.java
@@ -2,9 +2,16 @@ package com.star.spring_security_demo.service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoder;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.Data;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +19,26 @@ import java.util.Map;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET = "TV3U2VicmV02V5Rm9ySldUU2_nbmLyZ1B1cnBvc2VzMTIzNDU2Nzg=\r\n";
+
+    private String secretKey;
+
+    public JwtService() {
+        secretKey = generateSecretKey();
+    }
+
+    public String generateSecretKey() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey = keyGen.generateKey();
+            System.out.println("Secret Key: " + secretKey.toString());
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error generating secret key", e);
+        }
+    }
+
     public String generateToken(String username) {
 
         Map<String, Object> claims = new HashMap<> ();
@@ -19,10 +46,21 @@ public class JwtService {
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(username)
-                .setIssuedAt(new Data(System.currentTimeMillis()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000*60*3))
-                .signWith(getKey() , SignatureAlgorithm.HS256).compact();
+                .signWith(getKey() , SignatureAlgorithm.HS256)
+                .compact();
 
 
     }
+
+    private SecretKey getKey() {
+
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+
+    }
+
+
+
 }
